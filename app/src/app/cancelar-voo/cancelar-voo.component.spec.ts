544eaa6a162d1ed0b702e6b4dde8b266e1b090f8
Emanuelvtsg <evtsg@cin.ufpe.br>
2022-04-02 23:32:10 -0300

    criando basico cancelar

---
+++ app/src/app/cancelar-voo/cancelar-voo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CancelarVooComponent } from './cancelar-voo.component';
+
+describe('CancelarVooComponent', () => {
+  let component: CancelarVooComponent;
+  let fixture: ComponentFixture<CancelarVooComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CancelarVooComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CancelarVooComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/src/app/cancelar-voo/cancelar-voo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancelar-voo',
+  templateUrl: './cancelar-voo.component.html',
+  styleUrls: ['./cancelar-voo.component.css']
+})
+export class CancelarVooComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
