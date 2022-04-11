83c54dd32fb54d071b701bdd24ff4935fd3cda57
Emanuelvtsg <evtsg@cin.ufpe.br>
2022-04-11 19:58:23 -0300

    adicionando arquivo de fonte. campoSenha no TS

--- app/src/app/cancelar-voo/cancelar-voo.component.ts
+++ app/src/app/cancelar-voo/cancelar-voo.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+// import { Senha } from 'localOndeFicaraSenha';
 
 @Component({
   selector: 'app-cancelar-voo',
   templateUrl: './cancelar-voo.component.html',
   styleUrls: ['./cancelar-voo.component.css']
 })
-export class CancelarVooComponent implements OnInit {
 
-  constructor() { }
+export class CancelarVooComponent implements OnInit {
+  campoSenha: string;
+
+  constructor() { 
+    this.campoSenha = "";
+  }
+
+  limparCampoSenha(){
+    this.campoSenha = "";
+  }
 
   ngOnInit(): void {
   }
+
+  // senhaParaCancelar(senha: Senha): void {
+
+  //}
 
 }
