757c590e73e00480e1d3367852fa7c68fb48de5c
Gabriel de Oliveira Sousa <gos2@cin.ufpe.br>
2022-04-11 16:00:18 -0300

    entrega parcial

--- app/src/app/check-in/check-in.component.ts
+++ app/src/app/check-in/check-in.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-console.log("fdsa");
-
 @Component({
   selector: 'app-check-in',
   templateUrl: './check-in.component.html',
@@ -12,6 +10,43 @@ export class CheckInComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  show_modal(event: Event){
+    const confirm_button = document.querySelector<HTMLElement>(".check-in-confirm");
+    const modal_window = document.querySelector<HTMLElement>(".check-in-modal");
+
+    const value = (<HTMLInputElement>event.target).value;
+   
+    if (confirm_button != null){
+      (<HTMLInputElement>confirm_button).value = value;
+    } 
+    
+  
+    //console.log((<HTMLInputElement>confirm_button).value);
+    if (modal_window !== null) {
+      modal_window.style.display = 'block';
+    }
+    
+
+  }
+
+  hidden_modal(event: Event){
+    const buttons_first_confirm = document.querySelectorAll(".check-in-button-get");
+    const modal_window = document.querySelector<HTMLElement>(".check-in-modal");
+
+    buttons_first_confirm.forEach((item, ind) => {
+      if((<HTMLInputElement>item).value == (<HTMLInputElement>event.target).value){
+        (<HTMLElement>item).style.backgroundColor = 'gray';  
+      }
+    });
+
+    console.log((<HTMLInputElement>event.target).value);
+
+    if (modal_window !== null) {
+      modal_window.style.display = 'none';
+    }
+    
   }
 
 }
